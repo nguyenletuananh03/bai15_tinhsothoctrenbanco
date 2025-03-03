5b8e394a2c08ac469367f70f7fd9bb50cc9509bf
nguyenletuananh03 <phuc98181@gmail.com>
2025-03-03 09:17:53 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace bai15_tinhsothoctrenbanco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btntinh = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtso = new System.Windows.Forms.TextBox();
+            this.txtkq = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btntinh
+            // 
+            this.btntinh.Location = new System.Drawing.Point(317, 128);
+            this.btntinh.Name = "btntinh";
+            this.btntinh.Size = new System.Drawing.Size(75, 23);
+            this.btntinh.TabIndex = 0;
+            this.btntinh.Text = "tính ";
+            this.btntinh.UseVisualStyleBackColor = true;
+            this.btntinh.Click += new System.EventHandler(this.btntinh_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(332, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "số thóc trên bàn cờ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(219, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label1";
+            // 
+            // txtso
+            // 
+            this.txtso.Location = new System.Drawing.Point(305, 66);
+            this.txtso.Name = "txtso";
+            this.txtso.Size = new System.Drawing.Size(100, 20);
+            this.txtso.TabIndex = 2;
+            // 
+            // txtkq
+            // 
+            this.txtkq.Location = new System.Drawing.Point(305, 102);
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(100, 20);
+            this.txtkq.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtkq);
+            this.Controls.Add(this.txtso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btntinh);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btntinh;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtso;
+        private System.Windows.Forms.TextBox txtkq;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai15_tinhsothoctrenbanco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btntinh_Click(object sender, EventArgs e)
+        {
+            int o = Convert.ToInt32(txtso.Text);
+            double tongthoc = 0;
+
+            if (o > 64) 
+            {
+                txtso.Text = "Lỗi! trên bàn thờ chỉ có 0 đến 64 ô thôi!";
+            }
+            else
+            {
+                while (o > 64 || o < 0) ;
+                for (int i = 0; i < o; i++)
+                {
+                    tongthoc = tongthoc + Math.Pow(2, i);
+                }
+            }
+
+            txtkq.Text = tongthoc.ToString();
+
+        }
+    }
+}
